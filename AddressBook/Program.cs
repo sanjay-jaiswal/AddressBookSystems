1c42af35a826b086e10890dfc929df928c296c5a
sanjay jaiswal <sanjujaiswal.dev@gmail.com>
2021-04-28 21:10:31 +0530

    [Sanju] ADD: Ability to ensure no duplicate person in address book.

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -11,6 +11,51 @@ namespace AddressBook
             AddPersonDetails personDetailsOBJ = new AddPersonDetails();
             personDetailsOBJ.CreateMultipleUniqueAddressBook();
 
+
+/*
+
+            InterfaceDetails details = new AddPersonDetails();
+            bool check = true;
+            while (check == true)
+            {
+                Console.WriteLine("=============================Welcome ToAddress Book Program==========================");
+                Console.WriteLine("==========Please Enter Your Choice==========");
+                Console.WriteLine("1. Add Details");
+                Console.WriteLine("2. Display Details");
+                Console.WriteLine("3. Edit Details");
+                Console.WriteLine("4. Delete Details");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("=============================================");
+
+
+                string choice = Console.ReadLine();
+                int ch = Convert.ToInt32(choice);
+
+                switch (ch)
+                {
+                    case 1:
+                        details.AddDetails();
+                        break;
+                    case 2:
+                        details.Display();
+                        break;
+                    case 3:
+                        Console.WriteLine("Please Enter First Name : ");
+                        string name = Console.ReadLine();
+                        details.Edit(name);
+                        break;
+                    case 4:
+                        Console.WriteLine("Please Enter First Name : ");
+                        string nameForDeletion = Console.ReadLine();
+                        details.Delete(nameForDeletion);
+                        break;
+                    case 5:
+                        return;
+                }
+            }
+
+            */
+
         }
     }
 }
